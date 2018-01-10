1dac93bb2112989508581da955c0982bb29651f5
Maria Bermudez <bermudez.mt@gmail.com>
2018-01-09 16:33:25 -0800

    Update user agent header

--- flowroutenumbersandmessaging/controllers/base_controller.py
+++ flowroutenumbersandmessaging/controllers/base_controller.py
@@ -32,7 +32,7 @@ class BaseController(object):
     http_call_back = None
 
     global_headers = {
-        'user-agent': 'APIMATIC 2.0'
+        'user-agent': 'Flowroute SDK v3'
     }
 
     def __init__(self, client=None, call_back=None):
